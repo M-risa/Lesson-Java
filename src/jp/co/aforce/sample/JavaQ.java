784f44c6ab7af18907f22c670f449a1da5566fda
M-risa <risa.muraishi@acro-net.co.jp>
2026-04-28 13:29:21 +0900

    Java基礎演習① 完了

--- src/jp/co/aforce/sample/JavaQ.java
+++ src/jp/co/aforce/sample/JavaQ.java
@@ -42,36 +42,44 @@ public class JavaQ {
 	scanner.close();
 
 	
-	//問6
-	String names = "村石";
-    int age = 22;
-    String birthday = "2003-11-19";
+//	//問6
+//	String names = "村石";
+//    int age = 22;
+//    String birthday = "2003-11-19";
+//    
+//    String message = String.format("名前： %s, 年齢： %d歳, 誕生日： %s", names, age, birthday);
+//    
+//    System.out.println(message);
+////    
+//    
+//    //問7    
+//    System.out.print("商品の名前を入力してください： ");
+//    String itemName =  scanner.nextLine();
+//    
+//    System.out.print("単価（価格）を入力してください: ");
+//    int price = scanner.nextInt();
+//    
+//    System.out.print("購入個数を入力してください: ");
+//    int quantity = scanner.nextInt();
+//    
+//    int totalPrice = price * quantity;
+//    
+//    String result = String.format(
+//    		"商品名: %s ／ 単価: %d円 ／ 個数: %d個 ／ 合計金額: %d円",
+//    		itemName, price, quantity, totalPrice);
+//    
+//    System.out.println("--- 計算結果 ---");
+//    System.out.println(result);
+//
+//    scanner.close();
     
-    String message = String.format("名前： %s, 年齢： %d歳, 誕生日： %s", names, age, birthday);
     
-    System.out.println(message);
-    
-    
-    //問7    
-    System.out.print("商品の名前を入力してください： ");
-    String itemName =  scanner.nextLine();
-    
-    System.out.print("単価（価格）を入力してください: ");
-    int price = scanner.nextInt();
-    
-    System.out.print("購入個数を入力してください: ");
-    int quantity = scanner.nextInt();
-    
-    int totalPrice = price * quantity;
-    
-    String result = String.format(
-    		"商品名: %s ／ 単価: %d円 ／ 個数: %d個 ／ 合計金額: %d円",
-    		itemName, price, quantity, totalPrice);
-    
-    System.out.println("--- 計算結果 ---");
-    System.out.println(result);
+    //問
+    double x = 4.0;
+    double y = 2.0;
+    double z = Math.pow(x, y) - Math.sqrt(y);
+    System.out.println((int)z);
 
-    scanner.close();
 
 	}
 	
